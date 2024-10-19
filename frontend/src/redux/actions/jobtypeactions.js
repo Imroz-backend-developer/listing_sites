a054a969631821d76ed7369467e87f6aacc6377d
Imroz-backend-developer <shaikhimroz350@gmail.com>
2024-10-20 00:00:54 +0530

    Update jobtypeactions.js

--- frontend/src/redux/actions/jobtypeactions.js
+++ frontend/src/redux/actions/jobtypeactions.js
@@ -15,7 +15,7 @@ import {
 export const jobTypeLoadAction = () => async (dispatch) => {
     dispatch({ type: JOB_TYPE_LOAD_REQUEST });
     try {
-        const { data } = await axios.get('/api/type/jobs');
+        const { data } = await axios.get('https://listing-sites.onrender.com/api/type/jobs');
         dispatch({
             type: JOB_TYPE_LOAD_SUCCESS,
             payload: data
@@ -32,7 +32,7 @@ export const createJobTypeAction = (jobtype) => async (dispatch) => {
     dispatch({ type: CREATE_JOB_TYPE_REQUEST })
 
     try {
-        const { data } = await axios.post("/api/type/create", jobtype)
+        const { data } = await axios.post("https://listing-sites.onrender.com/api/type/create", jobtype)
         dispatch({
             type: CREATE_JOB_TYPE_SUCCESS,
             payload: data
